3b8ef99a70a4a19af11df5bec81bafd735d805b1
Guillem Poy <guplem@gmail.com>
2026-04-10 20:02:27 +0200

    Update app.py

--- src/ksp_mission_control/app.py
+++ src/ksp_mission_control/app.py
@@ -4,17 +4,18 @@ from textual.app import App, ComposeResult
 from textual.containers import Center, Middle
 from textual.widgets import Footer, Header, Static
 
-
 LOGO = r"""
- _  ______  ___   __  __ _         _               ___          _           _
-| |/ / ___||_ _| |  \/  (_)___ ___(_) ___  _ __   / __|___ _ _ | |_ _ _ ___| |
-| ' <\___ \ | |  | |\/| | (_-<(_-<| |/ _ \| '_ \ | (__/ _ \ ' \|  _| '_/ _ \ |
-|_|\_\____||___| |_|  |_|_/__//__/|_|\___/| .__/  \___\___/_||_|\__|_| \___/_|
-                                           |_|
-"""
+_  __  ____  ____    __  __ _          _              ____            _             _       / \
+| |/ / / ___||  _ \  |  \/  (_) ___ ___(_) ___  _ __  / ___|___  _ __ | |_ _ __ ___ | |     |   |
+| ' /  \___ \| |_) | | |\/| | / __/ __| |/ _ \| '_ \ | |   / _ \| '_ \| __| '__/ _ \| |     |   |
+| . \   ___) |  __/  | |  | | \__ \__ \ | (_) | | | || |__| (_) | | | | |_| | | (_) | |    /|   |\
+ |_|\_\ |____/|_|     |_|  |_|_|___/___/_|\___/|_| |_| \____\___/|_| |_|\__|_|  \___/|_|   /_|___|_\
+                                                                                            /_\
+                                                                                            |___|
+"""  # noqa: E501
 
 
-class WelcomeScreen(Static):
+class WelcomeView(Static):
     """Welcome screen shown on startup."""
 
     def compose(self) -> ComposeResult:
@@ -22,13 +23,6 @@ class WelcomeScreen(Static):
         yield Static("v0.1.0", id="version")
         yield Static("")
         yield Static("[b]Terminal Mission Control for Kerbal Space Program[/b]", id="tagline")
-        yield Static("")
-        yield Static(
-            "Press [b]d[/b] for demo mode  |  "
-            "Press [b]c[/b] to connect to KSP  |  "
-            "Press [b]q[/b] to quit",
-            id="instructions",
-        )
 
 
 class MissionControlApp(App[None]):
@@ -57,9 +51,8 @@ class MissionControlApp(App[None]):
         color: #005f1a;
     }
 
-    WelcomeScreen {
+    WelcomeView {
         width: 100%;
-        height: 100%;
         content-align: center middle;
         text-align: center;
     }
@@ -91,7 +84,7 @@ class MissionControlApp(App[None]):
 
     def compose(self) -> ComposeResult:
         yield Header()
-        yield Middle(Center(WelcomeScreen()))
+        yield Middle(Center(WelcomeView()))
         yield Footer()
 
     def action_demo(self) -> None:
