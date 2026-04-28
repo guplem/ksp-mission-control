18ea4861a14dcbdd5e86f880c2036027db0a13bf
Guillem Poy <guplem@gmail.com>
2026-04-28 22:26:49 +0200

    Update action.py

--- src/ksp_mission_control/control/actions/parachutes/action.py
+++ src/ksp_mission_control/control/actions/parachutes/action.py
@@ -34,10 +34,19 @@ class ParachutesAction(Action):
             param_type=ParamType.FLOAT,
             default=3_000,
         ),
+        ActionParam(
+            param_id="stage_for_parachutes",
+            label="Stage for Parachutes",
+            description="Should stage until parachutes are deployable? If true, will stage repeatedly until parachutes are deployable. If false, will not stage and attempt to deploy parachutes immediately, which may fail if they are not yet deployable.",
+            required=False,
+            param_type=ParamType.BOOL,
+            default=True,
+        ),
     ]
 
     def start(self, state: State, param_values: dict[str, Any]) -> None:
         self.min_altitude: float | None = param_values["min_altitude"]
+        self.stage_for_parachutes: bool = param_values["stage_for_parachutes"]
         pass
 
     def tick(self, state: State, commands: VesselCommands, dt: float, log: ActionLogger) -> ActionResult:
@@ -46,6 +55,10 @@ class ParachutesAction(Action):
                 status=ActionStatus.RUNNING,
                 message=f"Waiting to deploy paraparachutes until altitude is at or below {self.min_altitude}m (current altitude: {state.altitude_surface:.1f}m)",
             )
+
+        if False:  # TODO: stage until the current stage has one or more deployable parachutes, if self.stage_for_parachutes is True
+            commands.stage = True
+            return ActionResult(status=ActionStatus.RUNNING, message="Staging to reach deployable parachutes")
 
         commands.deployable_parachutes = True
         return ActionResult(status=ActionStatus.SUCCEEDED, message=f"Deploying paraparachutes at altitude {state.altitude_surface:.1f}m")
